b531cd3f20cc18d7fea5c0295bc4cdce59b0675d
siddwd <siddharthchaudharyy0508@gmail.com>
2024-07-26 09:25:03 +0530

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div>
-        <img src="https://cdn.pixabay.com/photo/2017/09/27/13/50/spiti-2792133_640.jpg" alt="" />
+    // <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="h-[40rem] w-full flex items-center justify-center  ">
+        <div className=" w-[60%] bg-white flex">
+        <img className="size-[24rem] w-[24rem]  ml-" src="https://media-cdn.tripadvisor.com/media/photo-o/27/f7/ff/6a/adventure-mountain.jpg" alt="" />
+        <p className="text-5xl ml-20 mt-5 ">THE PERFECT LOCATION FOR YOUR NEXT ADVENTURE</p>
       </div>
-    </main>
+      </div>
+    /* </main> */
   );
 }
