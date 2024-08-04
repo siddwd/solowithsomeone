f9966b808de3aa25b63b9e0d59eb21d85e62b402
siddwd <siddharthchaudharyy0508@gmail.com>
2024-08-04 12:19:27 +0530

    manali tsx

--- src/Pages/Manali.tsx
+++ src/Pages/Manali.tsx
@@ -1,10 +1,18 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import React from 'react'
 
 function Manali() {
   return (
     <>
-    <div className='h-80 w-full bg-white'>
-         <div className='p-5'>
+    
+        
+       
+    
+    <Carousel>
+  <CarouselContent>
+    <div className='h-80 bg-white flex flex-wrap'>
+    <CarouselItem>
+    <div className='p-5'>
             <p className='text-xl font-bold'>Best places of manali</p>
          </div>
         
@@ -16,8 +24,15 @@ function Manali() {
          </div>
           ))}
          </div>
-       
+      </CarouselItem>
     </div>
+    {/* <CarouselItem>...</CarouselItem> */}
+    {/* <CarouselItem>...</CarouselItem> */}
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
+
     </>
   )
 }
@@ -34,6 +49,18 @@ export const data = [
     {
         img:"https://assets.traveltriangle.com/blog/wp-content/uploads/2019/01/shutterstock_693251473.jpg",
         username:"Old manali",
+    },
+    {
+        img:"https://www.tripsavvy.com/thmb/fCRRyZwLNk9OJl4i1kbYjs3WBd4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-503016485-5ae56d61ba61770036315d22.jpg",
+        username:"Rafting ",
+    },
+    {
+        img:"https://www.tripsavvy.com/thmb/fCRRyZwLNk9OJl4i1kbYjs3WBd4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-503016485-5ae56d61ba61770036315d22.jpg",
+        username:"Rafting ",
+    },
+    {
+        img:"https://www.tripsavvy.com/thmb/fCRRyZwLNk9OJl4i1kbYjs3WBd4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-503016485-5ae56d61ba61770036315d22.jpg",
+        username:"Rafting ",
     },
     {
         img:"https://www.tripsavvy.com/thmb/fCRRyZwLNk9OJl4i1kbYjs3WBd4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-503016485-5ae56d61ba61770036315d22.jpg",
